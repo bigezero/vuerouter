337310dfe00b3f66cb6a2ebf11bc9ae968ac8b1b
bigezero <2415774867@qq.com>
2022-06-15 17:24:58 +0800

    initproject

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    host:'127.0.0.1',
+    port:3000,
+    open:true
+  },
+  lintOnSave:false
 })
